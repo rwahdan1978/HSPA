b6b74652c2a9afc82e247e3daedacb02936d40ed
rwahdan1978 <ramiwahdan1978@gmail.com>
2023-10-20 20:18:52 +0400

    updates 8pm 20-1-23

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,23 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PropertycardComponent } from './property/property-card/property-card..component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+
+const appRoutes: Routes = [
+  {path: '', component: PropertyListComponent},
+  {path: 'rent-property', component: AddPropertyComponent},
+  {path: 'add-property', component: AddPropertyComponent},
+  {path: 'property-detail/:id', component: PropertyDetailComponent}
+]
 
 @NgModule({
   declarations: [	
     AppComponent,
     PropertycardComponent,
     PropertyListComponent,
-      NavBarComponent
+      NavBarComponent,
+      AddPropertyComponent,
+      PropertyDetailComponent
    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     HousingService

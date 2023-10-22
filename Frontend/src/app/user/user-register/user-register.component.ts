ab472316a93c6dcc19adea555de4a7363b644abc
rwahdan1978 <ramiwahdan1978@gmail.com>
2023-10-22 15:01:15 +0400

    alertify done 22-10-2023

--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, ValidationErrors, AbstractControl, FormBuilder } from '@angular/forms';
 import { UserServiceService } from 'src/app/services/user-service.service';
 import { User } from 'src/app/model/user';
+import { AlertifyService } from 'src/app/services/alertify.service';
 
 @Component({
   selector: 'app-user-register',
@@ -15,7 +16,8 @@ export class UserRegisterComponent implements OnInit {
   user: User;
   userSubmitted: boolean;
 
-  constructor(private fb: FormBuilder, private UserServiceService: UserServiceService) { }
+  constructor(private fb: FormBuilder, private UserServiceService: UserServiceService,
+            private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.registerationForm = new FormGroup({
@@ -80,6 +82,10 @@ export class UserRegisterComponent implements OnInit {
       this.UserServiceService.addUser(this.userData());
       this.registerationForm.reset();
       this.userSubmitted = false;
+      this.alertify.success('You have registered successfully!');
+    }
+    else{
+      this.alertify.error('Please fill all required fields!');
     }
   
   }

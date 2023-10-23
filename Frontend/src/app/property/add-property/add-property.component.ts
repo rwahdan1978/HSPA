bd699eed34b314ef6df8d72a5784d3a013cf27d2
rwahdan1978 <ramiwahdan1978@gmail.com>
2023-10-24 01:46:36 +0400

    update 2AM 24-10-2023

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import {TabsetComponent} from 'ngx-bootstrap/tabs/public_api';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-add-property',
@@ -10,6 +12,19 @@ import { NgForm } from '@angular/forms';
 export class AddPropertyComponent implements OnInit {
 
   @ViewChild('Form') addPropertyForm: NgForm;
+  @ViewChild('formTabs') formTabs?: TabsetComponent;
+
+  propertyTypes: Array<string> = ['Home', "Apartment", "Duplix"];
+  furnishTypes: Array<string> = ['Fully', "Semi", "Unfirnished"];
+
+  //the errors below is not important as we need null for default numbers
+  propertyView: IProperty = {
+    Id: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    Type: ''
+  };
 
   constructor(private router: Router) { }
 
@@ -22,6 +37,12 @@ export class AddPropertyComponent implements OnInit {
 
   onSubmit(){
     console.log(this.addPropertyForm);
+  }
+
+  selectTab(tabId: number) {
+    if (this.formTabs?.tabs[tabId]) {
+      this.formTabs.tabs[tabId].active = true;
+    }
   }
 
 }

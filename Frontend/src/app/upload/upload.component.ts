d687758212c404d9e0bb8726d1ce06e6c2c288d4
rwahdan1978 <ramiwahdan1978@gmail.com>
2023-11-11 22:38:57 +0400

    updates 11-11-2023

--- Frontend/src/app/upload/upload.component.ts
+++ Frontend/src/app/upload/upload.component.ts
@@ -2,12 +2,14 @@ import { HttpClient, HttpEventType } from '@angular/common/http';
 import { Component, EventEmitter, Output } from '@angular/core';
 import {GetVariableService} from '../property/getVariable.service';
 import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
+
 export class UploadComponent {
 
   constructor(private http: HttpClient, private getVariable: GetVariableService) {}
@@ -17,6 +19,7 @@ export class UploadComponent {
   theFileName3: any;
   theFileName4: any;
   theFileName5: any;
+  base64Image: any;
   allfiles: Array<string> = [];
 
   setText1(theFileName1: string) {
@@ -52,6 +55,14 @@ export class UploadComponent {
 
           this.allfiles.push(droppedFile.relativePath);
 
+          const dlink: HTMLAnchorElement = document.createElement('a');
+          dlink.download = droppedFile.relativePath; // the file name
+          dlink.href = ".jpg, .jpeg, .png", droppedFile.relativePath;
+          dlink.download = droppedFile.relativePath;
+          document.body.appendChild(dlink);
+          dlink.click(); // this will trigger the dialog window
+          dlink.remove();
+
         });
       } else {
         // It was a directory (empty directories are added, otherwise only files)
@@ -64,8 +75,7 @@ export class UploadComponent {
       this.getVariable.theFileName3 = this.allfiles[2];
       this.getVariable.theFileName4 = this.allfiles[3];
       this.getVariable.theFileName5 = this.allfiles[4];
-
-
+      
     }
   }
 
@@ -76,4 +86,40 @@ export class UploadComponent {
   public fileLeave(event:any){
     console.log(event);
   }
+
+  getBase64ImageFromURL(url: string) {
+    return Observable.create((observer: Observer<string>) => {
+      const img: HTMLImageElement = new Image();
+      img.crossOrigin = "Anonymous";
+      img.src = url;
+      if (!img.complete) {
+        console.log(this.getBase64Image(img));
+        img.onload = () => {
+          observer.next(this.getBase64Image(img));
+          observer.complete();
+        };
+        img.onerror = err => {
+          observer.error(err);
+        };
+      } else {
+        observer.next(this.getBase64Image(img));
+        console.log(this.getBase64Image(img));
+        observer.complete();
+      }
+    });
+
+  }
+
+  getBase64Image(img: HTMLImageElement) {
+    console.log(this.getBase64Image(img));
+    const canvas: HTMLCanvasElement = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx: CanvasRenderingContext2D = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    const dataURL: string = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+  }
+
 }

f3d58d69d26cbe6ab5fe1eb7c1a80cd01a293e14
rwahdan1978 <ramiwahdan1978@gmail.com>
2023-11-16 12:30:27 +0400

    done with photos 16-11-2023

--- Frontend/src/app/property/property-detail/property-detail.component.ts
+++ Frontend/src/app/property/property-detail/property-detail.component.ts
@@ -13,8 +13,12 @@ export class PropertyDetailComponent implements OnInit {
 
 public propertyId: number;
 property: any = new Property();
+theArray: Array<any> = [];
 
   http: any;
+
+  visable1: boolean = false;
+  visable2: boolean = true;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -25,47 +29,17 @@ property: any = new Property();
       (data: any) => {
         this.property = data['prp'];
       }
-    );
+    )
   }
 
   showImage(image:String){
-    
-    let myImage1 = <HTMLElement> document.getElementById("image1"),
-    style1 = myImage1?.style || window.getComputedStyle(myImage1, "false"),
-    bi1 = style1.backgroundImage.slice(4, -1).replace(/"/g, "");
-    window.open(bi1,"_blank");
+    this.visable1 = true;
+    this.visable2 = false;
   }
 
-  showImage2(image:String){
-    
-    let myImage2 = <HTMLElement> document.getElementById("image2"),
-    style = myImage2?.style || window.getComputedStyle(myImage2, "false"),
-    bi2 = style.backgroundImage.slice(4, -1).replace(/"/g, "");
-    window.open(bi2,"_blank");
-  }
-
-  showImage3(image:String){
-    
-    let myImage3 = <HTMLElement> document.getElementById("image3"),
-    style = myImage3?.style || window.getComputedStyle(myImage3, "false"),
-    bi3 = style.backgroundImage.slice(4, -1).replace(/"/g, "");
-    window.open(bi3,"_blank");
-  }
-
-  showImage4(image:String){
-    
-    let myImage4 = <HTMLElement> document.getElementById("image4"),
-    style = myImage4?.style || window.getComputedStyle(myImage4, "false"),
-    bi4 = style.backgroundImage.slice(4, -1).replace(/"/g, "");
-    window.open(bi4,"_blank");
-  }
-
-  showImage5(image:String){
-    
-    let myImage5 = <HTMLElement> document.getElementById("image5"),
-    style = myImage5?.style || window.getComputedStyle(myImage5, "false"),
-    bi5 = style.backgroundImage.slice(4, -1).replace(/"/g, "");
-    window.open(bi5,"_blank");
+  hideImage(){
+    this.visable1 = false;
+    this.visable2 = true;
   }
     
 }

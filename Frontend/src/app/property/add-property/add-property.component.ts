d47200ad6fd0d5f5c26ee8c4f24608db578b6733
rwahdan1978 <ramiwahdan1978@gmail.com>
2023-12-30 20:28:19 +0400

    test

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -333,7 +333,7 @@ export class AddPropertyComponent implements OnInit {
         this.router.navigate(['/rent-property']);
       } else { 
           this.router.navigate(['/']);
-          location.reload(); 
+          //location.reload(); 
       }
 
     } else {

bb7ce551254299e997592c57681495d284e0ed29
rwahdan1978 <ramiwahdan1978@gmail.com>
2024-02-05 14:12:30 +0400

    update gitignore

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21f28bb22924bf201bc0268232207db17cf5582")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9dae7f1f82dcdd9c736db7c5a70018a745e35dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
